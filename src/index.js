8f750321986629cfab139d625bc7465b27b5a671
997try <997try@gmail.com>
2021-03-13 20:25:56 +0400

    feat: test success

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if( !array || array.length == 0 ){
+    return 0;
+  }else{
+    array.sort((a, b) => b - a).reverse();
+    return array[0];
+  }
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if( !array || array.length == 0){
+    return 0;
+  }else{
+    array.sort((a, b) => b - a);
+    return array[0];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length == 0){
+    return 0;
+  }else{
+    let sum = 0;
+    for( let i = 0; i < array.length; i++ ){
+      sum += parseInt( array[i], 10 );
+    }
+    let answer = sum/array.length;
+    return answer;
+
+  }
 }
